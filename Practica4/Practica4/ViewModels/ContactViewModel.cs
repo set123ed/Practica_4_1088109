0c8e08eb21fca77ebbffd9426b1640a6ac733289
set123ed <set123ed@gmail.com>
2021-03-10 23:56:23 -0400

    practica4

--- Practica4/Practica4/ViewModels/ContactViewModel.cs
+++ Practica4/Practica4/ViewModels/ContactViewModel.cs
@@ -1,10 +1,19 @@
 ﻿using System;
+using Practica4.Models;
+using Practica4.Views;
+using System.Collections.Generic;
 namespace Practica4.ViewModels
+
 {
     public class ContactViewModel
     {
+        public IList<Contact> Contacts { get; }
+
         public ContactViewModel()
         {
+            Contacts = new List<Contact>
+            { new Contact ( "Eduardo", "8093334444")};
+
         }
     }
 }
